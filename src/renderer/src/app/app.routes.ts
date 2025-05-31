a0d8403f611b899224061460437f5ddefd38d098
Guillaume Falvet <guillaume.falvet@outlook.com>
2025-05-31 17:33:25 +0200

    ✨ feat : Added the home component

--- src/renderer/src/app/app.routes.ts
+++ src/renderer/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./routes/home/home.component').then((render) => render.HomeComponent)
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
